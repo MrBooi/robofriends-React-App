69f20e1fbb7165aadffe68878063a76b747db05e
Mr Booi <ayabongabooi2@gmail.com>
2018-09-20 16:10:22 +0200

    PWA setup in react

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = ({name,email,id}) => {
  
     return(
      <div className= ' tc bg-light-green dib br3 ma2 grow bw2 shadow5'>
-       <img alt='robots' src={`https://robohash.org/${id}?25x50`} />
+       <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
        <div>
          <h2>{name}</h2>
          <p>{email}</p>
